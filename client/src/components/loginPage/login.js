12acfc5155ff483e4a9b3240f3bccb202d248656
aravindaka14 <aravindaka14@gmail.com>
2023-09-02 22:54:36 +0530

    bug removed

--- client/src/components/loginPage/login.js
+++ client/src/components/loginPage/login.js
@@ -18,7 +18,7 @@ const Login = () => {
             method: "POST",
             data: { email: loginState.email, password: loginState.password }
         }).then((response) => {
-            localStorage.setItem("userName", response.data.name)
+            localStorage.setItem("userName", response.data[0].name)
             navigate('/home')
         }).catch((err) => {
             // console.log(err.response.data)
