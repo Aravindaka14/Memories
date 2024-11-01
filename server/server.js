41db64f2ca3faf2217a5ec4df5e3b077a7c9abab
aravindaka14 <aravindaka14@gmail.com>
2024-11-01 11:43:14 +0530

    vercel.json configuration updated

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(mul.array())
 dotenv.config();
 app.use(cors({
-    origin:["https://memories-server-two.vercel.app/"],
+    origin:["https://memories-aravindak.vercel.app/"],
     methods:["GET","POST","PUT"],
     credentials:true
 }));    
